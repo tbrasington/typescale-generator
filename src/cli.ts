b8bf8c0d089afed8e393a5918008eb536162ddfd
Thomas Brasington <mail@tbrasington.com>
2022-07-08 23:16:20 +0100

    Add where to save

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 import kleur from "kleur";
-import { generateCSS, generateNamedScales, generateObject, TypographyScaleValues } from "./utils/scales";
-import fs from 'fs-extra'
+import {
+  generateCSS,
+  generateNamedScales,
+  generateObject,
+  TypographyScaleValues,
+} from "./utils/scales";
+import fs from "fs-extra";
+
 const questions = [
   {
-    type: "list",
+    type: "list", // replace with select https://github.com/SBoudrias/Inquirer.js/tree/master/packages/select
     name: "type_scale",
     message: "What type scale do you want to use?",
     choices: Object.keys(TypographyScaleValues).map(
@@ -16,13 +22,20 @@ const questions = [
       return val.split("(")[0].trim().toUpperCase().replace(" ", "_");
     },
   },
+
+  {
+    type: "input",
+    name: "file",
+    message: "Where should we save the files?",
+    default: "./",
+  },
 ];
 
 export function cli() {
   inquirer.prompt(questions).then((answers) => {
     console.log(JSON.stringify(answers, null, "  "));
 
-    const ScaleValues = generateNamedScales(answers.type_scale).typeScale
+    const ScaleValues = generateNamedScales(answers.type_scale).typeScale;
     // these all need to be options that toggle depending on choices
 
     // generate JS/TS object justing using the min/max values
@@ -32,28 +45,29 @@ export function cli() {
     // generate a JSON file following the design tokens spec
 
     // generate object for JS/TS users using CSS
-    const typeSteps = generateObject({ scales: ScaleValues })
-    console.log(typeSteps);
-    
-    
-    const jsFile = 'samples/type-scale.ts'
- 
-    fs.outputFile(jsFile, `export const typeScale = ${JSON.stringify(typeSteps, null, "  ")}`)
+    const typeSteps = generateObject({ scales: ScaleValues });
+    //console.log(typeSteps);
+
+    const jsFile = `${answers.file}/index.js`;
+
+    fs.outputFile(
+      jsFile,
+      `export const typeScale = ${JSON.stringify(typeSteps, null, "  ")}`
+    )
       .then(() => fs.readFile(jsFile, "utf8"))
       .then((data) => {
-        console.log(data); // => hello!
+        //console.log(data); // => hello!
       })
       .catch((err) => {
         console.error(err);
       });
 
-
-    const typeStepsCSS = generateCSS({ scales: ScaleValues })
-    const cssFile = 'samples/type-scale.css'
+    const typeStepsCSS = generateCSS({ scales: ScaleValues });
+    const cssFile = `${answers.file}/index.css`;
     fs.outputFile(cssFile, typeStepsCSS)
       .then(() => fs.readFile(cssFile, "utf8"))
       .then((data) => {
-        console.log(data); // => hello!
+        //console.log(data); // => hello!
       })
       .catch((err) => {
         console.error(err);
