6b8c37fa57dbb57e67a0d8fd1fdf9f74e167a9c5
Thomas Brasington <mail@tbrasington.com>
2022-07-08 22:43:46 +0100

    Output a JS object

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 import kleur from "kleur";
-import { generateScales, generateTypeScript, TypographyScaleValues } from "./utils/scales";
+import { generateNamedScales, generateObject, TypographyScaleValues } from "./utils/scales";
 import fs from 'fs-extra'
 const questions = [
   {
@@ -21,12 +21,24 @@ const questions = [
 export function cli() {
   inquirer.prompt(questions).then((answers) => {
     console.log(JSON.stringify(answers, null, "  "));
-    console.log(generateTypeScript({scales: generateScales().typeScale}));
+
+    const ScaleValues = generateNamedScales(answers.type_scale).typeScale
+    // these all need to be options that toggle depending on choices
+
+    // generate JS/TS object justing using the min/max values
+
+    // generate a CSS file using the clamp values + a fall back with breakpoints
+
+    // generate a JSON file following the design tokens spec
+
+    // generate object for JS/TS users using CSS
+    const typeSteps = generateObject({ scales: ScaleValues })
+    console.log(typeSteps);
     
     
     const file = 'samples/type-scale.ts'
 
-    fs.outputFile(file, "hello!")
+    fs.outputFile(file, `export const typeScale = ${JSON.stringify(typeSteps, null, "  ")}`)
       .then(() => fs.readFile(file, "utf8"))
       .then((data) => {
         console.log(data); // => hello!
--- src/utils/scales.ts
+++ src/utils/scales.ts
@@ -1,22 +1,27 @@
 
 
-interface viewPortProps {
+interface ViewPortProps {
   width: number;
   fontSize: number;
   typeScale: number;
 }
 
-interface SystemProps {
-  typeScale: systemItemProps[];
+interface GeneratedNamedScalesProps {
+  typeScale: GeneratedItemProps[];
 }
 
-interface systemItemProps {
+interface GeneratedItemProps {
   min: number;
   max: number;
   clamp: string;
 }
 
-export const SCALES = {
+
+interface GenericKeyValueProps { 
+  [key: string]: string 
+}
+
+export const NamedScales = {
   MAJOR_SECOND: "MAJOR_SECOND",
   MINOR_SECOND: "MINOR_SECOND",
   MAJOR_THIRD: "MAJOR_THIRD",
@@ -31,22 +36,28 @@ export const SCALES = {
   MAJOR_SEVENTH: "MAJOR_SEVENTH",
 };
 
+
+/*
+* Types for scale enums
+*/
+type NamedScalesProps = (typeof NamedScales)[keyof typeof NamedScales]
+
 export const TypographyScaleValues = {
-  [SCALES.MINOR_SECOND]: { name: "Minor second", value: 1.067 },
-  [SCALES.MAJOR_SECOND]: { name: "Major second", value: 1.125 },
-  [SCALES.MINOR_THIRD]: { name: "Minor third", value: 1.2 },
-  [SCALES.MAJOR_THIRD]: { name: "Major third", value: 1.25 },
-  [SCALES.PERFECT_FOURTH]: { name: "Perfect fourth", value: 1.333 },
-  [SCALES.AUGMENTED_FOURTH]: {
+  [NamedScales.MINOR_SECOND]: { name: "Minor second", value: 1.067 },
+  [NamedScales.MAJOR_SECOND]: { name: "Major second", value: 1.125 },
+  [NamedScales.MINOR_THIRD]: { name: "Minor third", value: 1.2 },
+  [NamedScales.MAJOR_THIRD]: { name: "Major third", value: 1.25 },
+  [NamedScales.PERFECT_FOURTH]: { name: "Perfect fourth", value: 1.333 },
+  [NamedScales.AUGMENTED_FOURTH]: {
     name: "Augmented fourth",
     value: 1.414,
   },
-  [SCALES.PERFECT_FIFTH]: { name: "Perfect fifth", value: 1.5 },
-  [SCALES.GOLDEN_RATIO]: { name: "Golden ratio", value: 1.618 },
-  [SCALES.MAJOR_SIXTH]: { name: "Major sixth", value: 1.667 },
-  [SCALES.MINOR_SEVENTH]: { name: "Minor seventh", value: 1.778 },
-  [SCALES.MAJOR_SEVENTH]: { name: "Major seventh", value: 1.875 },
-  [SCALES.OCTAVE]: { name: "Octave", value: 2 },
+  [NamedScales.PERFECT_FIFTH]: { name: "Perfect fifth", value: 1.5 },
+  [NamedScales.GOLDEN_RATIO]: { name: "Golden ratio", value: 1.618 },
+  [NamedScales.MAJOR_SIXTH]: { name: "Major sixth", value: 1.667 },
+  [NamedScales.MINOR_SEVENTH]: { name: "Minor seventh", value: 1.778 },
+  [NamedScales.MAJOR_SEVENTH]: { name: "Major seventh", value: 1.875 },
+  [NamedScales.OCTAVE]: { name: "Octave", value: 2 },
 };
 
 function round(num: number) {
@@ -77,47 +88,19 @@ function clampBuilder(
   return `clamp(${min}, ${val}, ${max})`;
 }
 
-export function generateCSS(system: { typeScale: any }) {
-  let css = "";
-
-  const { typeScale } = system;
-
-  for (const step of Object.keys(typeScale)) {
-    css += `--step-${step}: ${typeScale[step].clamp};`;
-  }
-  return css;
-}
-
-
-interface cssProps { 
-  [key: string]: string 
-}
-
-export function generateTypeScript({ scales}  : {scales: systemItemProps[]}) {
-
-
-const typeSteps : cssProps = {} 
-
-  for (const step of Object.keys(scales)) {
-    typeSteps[`step-${step}`] = scales[parseInt(step)].clamp
-  }
-
-  return typeSteps
-}
 export function buildFluidDesignSystem(opts: {
-  typeScaleSteps: number[];
-  minViewport: viewPortProps;
-  maxViewport: viewPortProps;
+  range: number[];
+  minViewport: ViewPortProps;
+  maxViewport: ViewPortProps;
 }) {
-  const { minViewport, maxViewport, typeScaleSteps } = opts;
+  const { minViewport, maxViewport, range } = opts;
 
-  const system: SystemProps = {
+  const system: GeneratedNamedScalesProps = {
     typeScale: [],
   };
 
-  const minFluidTypeStep = typeScaleSteps[0];
-  const maxFluidTypeStep = typeScaleSteps[typeScaleSteps.length - 1];
-
+  const minFluidTypeStep = range[0];
+  const maxFluidTypeStep = range[range.length - 1];
   for (let i = minFluidTypeStep; i <= maxFluidTypeStep; i++) {
     const valueMin = round(
       minViewport.fontSize * Math.pow(minViewport.typeScale, i)
@@ -144,23 +127,71 @@ export function buildFluidDesignSystem(opts: {
   };
 }
 
-export function generateScales() {
+
+
+export function generateCSS(system: { typeScale: any }) {
+  let css = "";
+
+  const { typeScale } = system;
+
+  for (const step of Object.keys(typeScale)) {
+    css += `--step-${step}: ${typeScale[step].clamp};`;
+  }
+  return css;
+}
+
+
+
+/**
+ * @returns An object of CSS clamped values, corresponding to their step.
+ * @example ```js
+ * {
+      'step-0': 'clamp(1rem, 0.93rem + 0.36vw, 1.25rem)',
+      'step-1': 'clamp(1.62rem, 1.5rem + 0.58vw, 2.02rem)',
+      'step-2': 'clamp(2.62rem, 2.43rem + 0.93vw, 3.27rem)',
+      'step-3': 'clamp(4.24rem, 3.93rem + 1.51vw, 5.3rem)',
+      'step-4': 'clamp(6.85rem, 6.36rem + 2.45vw, 8.57rem)',
+      'step-5': 'clamp(11.09rem, 10.3rem + 3.96vw, 13.86rem)',
+      'step--2': 'clamp(0.38rem, 0.35rem + 0.14vw, 0.48rem)',
+      'step--1': 'clamp(0.62rem, 0.57rem + 0.22vw, 0.77rem)'
+    }
+ * ```
+ */
+export function generateObject({ scales}  : {scales: GeneratedItemProps[]}) {
+
+
+const typeSteps : GenericKeyValueProps = {} 
+
+  for (const step of Object.keys(scales)) {
+    typeSteps[`step-${step}`] = scales[parseInt(step)].clamp
+  }
+
+  return typeSteps
+}
+
+/**
+ * Searches through chained references to replace reference with originating value
+ * @param scale the scale to use for generation
+ * @returns A generated set of scales using the specified scale, along with the min max sizes for a breakpoint
+ */
+export function generateNamedScales(scale : NamedScalesProps) {
+
   const config = {
     minViewport: {
       width: 320,
       fontSize: 16,
-      typeScale: TypographyScaleValues[SCALES.MINOR_SECOND].value,
+      typeScale: TypographyScaleValues[scale].value,
     },
     maxViewport: {
       width: 1440,
       fontSize: 20,
-      typeScale: TypographyScaleValues[SCALES.MINOR_SECOND].value,
+      typeScale: TypographyScaleValues[scale].value,
     },
   };
 
   const fluidDesignSystem = buildFluidDesignSystem({
     ...config,
-    typeScaleSteps: [-2, -1, 0, 1, 2, 3, 4, 5],
+    range: [-2, -1, 0, 1, 2, 3, 4, 5],
   });
 
   return fluidDesignSystem;
