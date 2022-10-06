c0b11f9a95efedb2af63892b31fabb579fdf5c1b
Thomas Brasington <mail@tbrasington.com>
2022-10-06 21:57:45 +0100

    load the fonts up front and filter out any of the unloaded ones

--- apps/figma/src/main.ts
+++ apps/figma/src/main.ts
@@ -23,45 +23,80 @@ export default function () {
         style: "Regular",
       });
     }
-    console.log({ fontsToLoad });
+
     return Promise.all(
-      fontsToLoad.map(async (font) => await figma.loadFontAsync(font))
+      fontsToLoad.map(
+        async (font) =>
+          await figma
+            .loadFontAsync(font)
+            .then(() => {
+              return { font, status: "loaded" };
+            })
+            .catch(() => {
+              return { font, status: "failed" };
+            })
+      )
     );
   }
 
   on<InsertCodeHandler>("INSERT_CODE", async function () {
-    await loadAllFonts();
+    await loadAllFonts()
+      .then((status) => {
+        console.log({ status });
 
-    for (const property in textStyles.styles) {
-      // get the text style values
-      const textStyle = textStyles.styles[property as StyleProps];
+        const textStylestoParse = textStyles.styles;
+        // loop through
+        for (const property in textStylestoParse) {
+          // get the text style values
+          const textStyle = textStyles.styles[property as StyleProps];
 
-      // do the ting
-      const exisitingStyles = figma.getLocalTextStyles();
+          const failedLoad = status.find(
+            (item) =>
+              item.font.family === textStyle.$value.fontFamily &&
+              item.status === "failed"
+          );
 
-      const matchedStyle = exisitingStyles.find(
-        (style) =>
-          style.id ===
-          style.getPluginData(`${textStyles.namespace}-${property}`)
-      );
-      // if we have a match, lets update it rather than dupe it
-      const style = matchedStyle ? matchedStyle : figma.createTextStyle();
+          console.log({ font: textStyle.$value.fontFamily, failedLoad });
+          if (failedLoad) {
+            figma.notify(
+              `We couldn't load in ${textStyle.$value.fontFamily} on the style ${property}`
+            );
+          } else {
+            // do the ting
+            const exisitingStyles = figma.getLocalTextStyles();
 
-      // set the name
-      style.name = property;
-      style.fontName = {
-        family: textStyle.$value.fontFamily,
-        style: "Regular",
-      };
+            const matchedStyle = exisitingStyles.find(
+              (style) =>
+                style.id ===
+                style.getPluginData(`${textStyles.namespace}-${property}`)
+            );
+            // if we have a match, lets update it rather than dupe it
+            const style = matchedStyle ? matchedStyle : figma.createTextStyle();
 
-      // set the font size
-      style.fontSize = textStyle.$value.fontSize;
+            // set the name
+            style.name = property;
+            style.fontName = {
+              family: textStyle.$value.fontFamily,
+              style: "Regular",
+            };
 
-      // save the id for later usage
-      if (matchedStyle === undefined) {
-        style.setPluginData(`${textStyles.namespace}-${property}`, style.id);
-      }
-    }
+            // set the font size
+            style.fontSize = textStyle.$value.fontSize;
+
+            // save the id for later usage
+            if (matchedStyle === undefined) {
+              style.setPluginData(
+                `${textStyles.namespace}-${property}`,
+                style.id
+              );
+            }
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
     figma.closePlugin();
   });
   showUI({ width: 320, height: 240 });
