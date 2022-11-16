e43fc4ca5494852817c35f096105154a1a7d5c9c
Thomas Brasington <mail@tbrasington.com>
2022-11-16 18:07:56 +0000

    fix local docs

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -8,16 +8,18 @@ export default function Docs() {
 
   const scale = buildTypographyScales({
     range: [-1, 0, 2],
-    min: {
-      width: 320,
-      fontSize: 16,
-      typeScale: TypographyScaleValues.MINOR_SECOND.value,
-    },
-    max: {
-      width: 1440,
-      fontSize: 20,
-      typeScale: TypographyScaleValues.PERFECT_FOURTH.value,
-    },
+    sizes: [
+      {
+        width: 320,
+        fontSize: 16,
+        typeScale: TypographyScaleValues.MINOR_SECOND.value,
+      },
+      {
+        width: 1440,
+        fontSize: 20,
+        typeScale: TypographyScaleValues.PERFECT_FOURTH.value,
+      },
+    ],
   });
   return (
     <div>
